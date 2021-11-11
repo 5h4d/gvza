3501748b4582afe49de00451a8253e9a4824b6f5
Damián Chaban <dch0@duck.com>
2021-11-11 19:48:45 +0100

    Python Sexta

--- Sexta/BlackJack.py
+++ Sexta/BlackJack.py
@@ -18,6 +18,8 @@ def bet():
     cnv.create_rectangle(120, 70, 180, 130, fill='white', outline='white')
     cnv.create_rectangle(120,285,180,345,fill='white',outline='white')
     cnv.create_rectangle(130,190,170,210,fill='white',outline='white')
+    cnv.create_rectangle(160, 370, 198, 398, fill='white', outline='white')
+    cnv.create_text(185, 393, text=bal)
 
 def act():
     cnv.create_rectangle(35, 425, 265, 475, outline='white', fill='white')
@@ -27,14 +29,15 @@ def act():
     cnv.create_text(150, 452, text='Stand', font='arial 20', fill='green')
     cnv.create_rectangle(195, 425, 265, 475, outline='green')
     cnv.create_text(230, 450, text='Double Down', font='arial 9', fill='green', angle=35)
-bet()
+
 bal=500
+bet()
 ctrl=0
 def igra(xy):
     global bal,ctrl,psum,dsum,tab
     x=xy.x
     y=xy.y
-    if ctrl==0:
+    if ctrl==0 and bal>=0:
         bet()
         cnv.create_rectangle(120, 70, 180, 130, fill='white', outline='white')
         cnv.create_rectangle(120,285,180,345,fill='white',outline='white')
@@ -54,7 +57,7 @@ def igra(xy):
         if tab!=0:
             ctrl+=1
             cnv.create_rectangle(120, 180, 180, 220, fill='white', outline='white')
-            cnv.create_rectangle(160, 370, 190, 398, fill='white', outline='white')
+            cnv.create_rectangle(160, 370, 198, 398, fill='white', outline='white')
             cnv.create_text(150, 200, text=tab)
             cnv.create_text(185, 393, text=bal)
             act()
@@ -62,7 +65,7 @@ def igra(xy):
             dsum=rnd.randint(1,11)
             cnv.create_text(150,100,text=dsum,font='arial 20 bold')
             cnv.create_text(150,315,text=psum,font='arial 20 bold')
-    elif ctrl==1:
+    elif ctrl==1 and bal>=0:
         if y>=425 and y<=475:
             if x>=35 and x<=105:
                 psum+=rnd.randint(1,10)
@@ -75,6 +78,36 @@ def igra(xy):
                 if dsum>=17:
                     if psum>>dsum or dsum>=22:
                         bal+=tab*2
+                        tab=0
+                        ctrl-=1
+                        bet()
+                    elif psum==dsum:
+                        bal+=tab
+                        tab=0
+                        ctrl-=1
+                        bet()
+                    else:
+                        tab=0
+                        ctrl-=1
+                        bet()
+                else:
+                    dsum+=rnd.randint(1,10)
+                    xy.x=120
+                    xy.y=430
+                    igra(xy)
+            if x>=195 and x<=265:
+                bal-=tab
+                tab*=2
+                psum+=rnd.randint(1,10)
+                dsum+=rnd.randint(1,10)
+                if dsum>=17:
+                    if psum>>dsum or dsum>=22:
+                        bal+=tab*2
+                        tab=0
+                        ctrl-=1
+                        bet()
+                    elif psum==dsum:
+                        bal+=tab
                         tab=0
                         ctrl-=1
                         bet()
@@ -92,7 +125,7 @@ def igra(xy):
         cnv.create_rectangle(120,285,180,345,fill='white',outline='white')
         cnv.create_text(150,100,text=dsum,font='arial 20 bold')
         cnv.create_text(150,315,text=psum,font='arial 20 bold')
-        cnv.create_rectangle(160, 370, 190, 398, fill='white', outline='white')
+        cnv.create_rectangle(160, 370, 198, 398, fill='white', outline='white')
         cnv.create_text(185, 393, text=bal)
                     
                     
