3e1366bdf938260ffca5f6a33d6559334ae0506a
Damián Chaban <dch0@duck.com>
2021-11-18 10:20:45 +0100

    Python Sexta

---
+++ Sexta/Tvary na základe pozíce.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+c = tk.Canvas(height=400, width=400)
+c.pack()
+
+c.create_line(0, 200, 400, 200, width=4)
+
+def bruh(xy):
+    x=xy.x
+    y=xy.y
+    if y<200:
+        c.create_rectangle(x, y, x+20, y+20, fill='blue', outline='blue',tags='yes')
+    else:
+        c.create_oval(x, y, x+20, y+20, fill='yellow', outline='yellow', tags='yes')
+
+def gon(nah):
+    c.delete('yes')
+
+c.bind('<Button-1>',bruh)
+c.bind_all('r',gon)
