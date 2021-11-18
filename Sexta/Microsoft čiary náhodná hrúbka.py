28fc5b47465ee70df16c312bda9364db42bfa90d
Damián Chaban <dch0@duck.com>
2021-11-18 10:30:48 +0100

    Python Sexta

--- Sexta/Microsoft čiary náhodná hrúbka.py
+++ Sexta/Microsoft čiary náhodná hrúbka.py
@@ -7,12 +7,12 @@ def cz(xy):
     x=xy.x
     y=xy.y
     if x<200 and y<200:
-        c.create_line(200, 200, x, y, fill='red', width=random.randint(0,9))
+        c.create_line(200, 200, x, y, fill='red', width=random.randint(1,9))
     if x<200 and y>200:
-        c.create_line(200, 200, x, y, fill='blue', width=random.randint(0,9))
+        c.create_line(200, 200, x, y, fill='blue', width=random.randint(1,9))
     if x>200 and y<200:
-        c.create_line(200,200,x,y,fill='green', width=random.randint(0,9))
+        c.create_line(200,200,x,y,fill='green', width=random.randint(1,9))
     if x>200 and y>200:
-        c.create_line(200,200,x,y,fill='yellow',width=random.randint(0,9))
+        c.create_line(200,200,x,y,fill='yellow',width=random.randint(1,9))
 
 c.bind('<Button-1>',cz)
