bcf7ad1a34e2e657f11256f800fea35de0353f7c
Damián Chaban <mac@MacBookArch.localdomain>
2022-02-10 12:22:12 +0100

    WIP

--- Sexta/Pokus o míny.py
+++ Sexta/Pokus o míny.py
@@ -130,30 +130,9 @@ if rnd.randrange(2)==1:
     ef=1
 else:
     ef=0
-if rnd.randrange(2)==1:
-    fa=1
-else:
-    fa=0
-if rnd.randrange(2)==1:
-    fb=1
-else:
-    fb=0
-if rnd.randrange(2)==1:
-    fc=1
-else:
-    fc=0
-if rnd.randrange(2)==1:
-    fd=1
-else:
-    fd=0
-if rnd.randrange(2)==1:
-    fe=1
-else:
-    fe=0
-if rnd.randrange(2)==1:
-    ff=1
-else:
-    ff=0
+def hiha():
+    c.delete('all')
+    c.create_text(100,120,text='L',font='arial 40')
 
 def yes(xy):
     x=xy.x
@@ -161,6 +140,183 @@ def yes(xy):
     if x<40:
         if y<40:
             if aa==0:
-                saa=ba+bb+ab
-                c.create_text(20,20,text=saa)
+                ctl=ba+bb+ab
+                c.create_text(20,20,text=ctl)
+            else:
+                hiha()
+        elif y<80:
+            if ab==0:
+                ctl=aa+ac+ba+bb+bc
+                c.create_text(20,60,text=ctl)
+            else:
+                hiha()
+        elif y<120:
+            if ac==0:
+                ctl=ab+ad+bb+bc+bd
+                c.create_text(20,100,text=ctl)
+            else:
+                hiha()
+        elif y<160:
+            if ad==0:
+                ctl=ac+ae+bc+bd+be
+                c.create_text(20,140,text=ctl)
+            else:
+                hiha()
+        elif y<200:
+            if ae==0:
+                ctl=ad+af+bd+be+bf
+                c.create_text(20,180,text=ctl)
+            else:
+                hiha()
+        elif y<240:
+            if af==0:
+                ctl=ae+be+bf
+                c.create_text(20,220,text=ctl)
+            else:
+                hiha()
+    elif x<80:
+        if y<40:
+            if ba==0:
+                ctl=aa+ab+bb+ca+cb
+                c.create_text(60,20,text=ctl)
+            else:
+                hiha
+        elif y<80:
+            if bb==0:
+                ctl=aa+ab+ac+ba+bc+ca+cb+cc
+                c.create_text(60,60,text=ctl)
+            else:
+                hiha()
+        elif y<120:
+            if bc==0:
+                ctl=ab+ac+ad+bb+bd+cb+cc+cd
+                c.create_text(60,100,text=ctl)
+            else:
+                hiha()
+        elif y<160:
+            if bd==0:
+                ctl=ac+ad+ae+bc+be+cc+cd+ce
+                c.create_text(60,140,text=ctl)
+            else:
+                hiha()
+        elif y<200:
+            if be==0:
+                ctl=ad+ae+af+bd+bf+cd+ce+cf
+                c.create_text(60,180,text=ctl)
+            else:
+                hiha()
+        elif y<240:
+            if bf==0:
+                ctl=ae+af+be+ce+cf
+                c.create_text(60,220,text=ctl)
+    elif x<120:
+        if y<40:
+            if ca==0:
+                ctl=ba+bb+cb+da+db
+                c.create_text(100,20,text=ctl)
+            else:
+                hiha()
+        elif y<80:
+            if cb==0:
+                ctl=ba+bb+bc+ca+cc+da+db+dc
+                c.create_text(100,60,text=ctl)
+            else:
+                hiha()
+        elif y<120:
+            if cc==0:
+                ctl=bb+bc+bd+cb+cd+db+dc+dd
+                c.create_text(100,100,text=ctl)
+            else:
+                hiha()
+        elif y<160:
+            if cd==0:
+                ctl=bc+bd+be+cc+ce+dc+dd+de
+                c.create_text(100,140,text=ctl)
+            else:
+                hiha()
+        elif y<200:
+            if ce==0:
+                ctl=bd+be+bf+cd+cf+dd+de+df
+                c.create_text(100,180,text=ctl)
+            else:
+                hiha()
+        elif y<240:
+            if cf==0:
+                ctl=be+bf+ce+de+df
+                c.create_text(100,220,text=ctl)
+    elif x<160:
+        if y<40:
+            if da==0:
+                ctl=ca+cb+db+ea+eb
+                c.create_text(140,20,text=ctl)
+            else:
+                hiha()
+        elif y<80:
+            if db==0:
+                ctl=ca+cb+cc+da+dc+ea+eb+ec
+                c.create_text(140,60,text=ctl)
+            else:
+                hiha()
+        elif y<120:
+            if dc==0:
+                ctl=cb+cc+cd+db+dd+eb+ec+ed
+                c.create_text(140,100,text=ctl)
+            else:
+                hiha()
+        elif y<160:
+            if dd==0:
+                ctl=cc+cd+ce+dc+de+ec+ed+ee
+                c.create_text(140,140,text=ctl)
+            else:
+                hiha()
+        elif y<200:
+            if de==0:
+                ctl=cd+ce+cf+dd+df+ed+ee+ef
+                c.create_text(140,180,text=ctl)
+            else:
+                hiha()
+        elif y<240:
+            if df==0:
+                ctl=ce+cf+de+ee+ef
+                c.create_text(140,220,text=ctl)
+    elif x<200:
+        if y<40:
+            if ea==0:
+                ctl=da+db+eb
+                c.create_text(180,20,text=ctl)
+            else:
+                hiha()
+        elif y<80:
+            if eb==0:
+                ctl=ea+ec+da+db+dc
+                c.create_text(180,60,text=ctl)
+            else:
+                hiha()
+        elif y<120:
+            if ec==0:
+                ctl=eb+ed+db+dc+dd
+                c.create_text(180,100,text=ctl)
+            else:
+                hiha()
+        elif y<160:
+            if ed==0:
+                ctl=ec+ee+dc+dd+de
+                c.create_text(180,140,text=ctl)
+            else:
+                hiha()
+        elif y<200:
+            if ee==0:
+                ctl=ed+ef+dd+de+df
+                c.create_text(180,180,text=ctl)
+            else:
+                hiha()
+        elif y<240:
+            if ef==0:
+                ctl=ee+de+df
+                c.create_text(180,220,text=ctl)
+            else:
+                hiha()
+def no(xy):
+    
 c.bind("<Button-1>",yes)
+c.bind("<Button-2>",no)
