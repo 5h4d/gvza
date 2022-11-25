4720f8fd0e3d95e7b7db95412d0aedfaa5f56c4e
Damián Chaban <mac@MacBookArch.localdomain>
2022-11-25 14:00:01 +0100

    25 Nov 22

--- Seminár/kopec a info o ňom a tak.py
+++ Seminár/kopec a info o ňom a tak.py
@@ -6,21 +6,18 @@ c.pack()
 
 v=n=d=x=0
 y=randrange(270)
-while True:
-    if x<500:
-        xx=x+randrange(100)
-        yy=randrange(270)
-        c.create_line(x,y,xx,yy)
-        if yy>y:
-            n+=yy-y
-            d+=sqrt(((xx-x)**2)+((yy-y)**2))
-        else:
-            v+=y-yy
-            d+=sqrt(((xx-x)**2)+((y-yy)**2))
-        x=xx
-        y=yy
+while x<500:
+    xx=x+randrange(100)
+    yy=randrange(270)
+    c.create_line(x,y,xx,yy)
+    if yy>y:
+        n+=yy-y
+        d+=sqrt(((xx-x)**2)+((yy-y)**2))
     else:
-        break
+        v+=y-yy
+        d+=sqrt(((xx-x)**2)+((y-yy)**2))
+    x=xx
+    y=yy
 c.create_text(50,270,text='Stúpanie: '+str(v)+'m')
 c.create_text(48,280,text='Klesanie: '+str(n)+'m')
 c.create_text(74,290,text='Prejdená cesta: '+str(d//1)+'m')
