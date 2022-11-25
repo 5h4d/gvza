475b94247eef71a32b2f30d3ecbe6c1cabbd3320
Damián Chaban <mac@MacBookArch.localdomain>
2022-11-25 13:48:27 +0100

    25 Nov 22

---
+++ Seminár/kopec a info o ňom a tak.py
@@ -0,0 +1,26 @@
+from random import randrange
+from math import sqrt
+import tkinter as tk
+c=tk.Canvas(height=300,width=500)
+c.pack()
+
+v=n=d=x=0
+y=randrange(300)
+while True:
+    if x<500:
+        xx=x+randrange(100)
+        yy=randrange(300)
+        c.create_line(x,y,xx,yy)
+        if yy>y:
+            v+=yy-y
+            d+=sqrt(((xx-x)**2)+((yy-y)**2))
+        else:
+            n+=y-yy
+            d+=sqrt(((xx-x)**2)+((y-yy)**2))
+        x=xx
+        y=yy
+    else:
+        break
+c.create_text(50,270,text='Stúpanie: '+str(v)+'m')
+c.create_text(48,280,text='Klesanie: '+str(n)+'m')
+c.create_text(74,290,text='Prejdená cesta: '+str(d//1)+'m')
