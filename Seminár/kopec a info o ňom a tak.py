fb7981ff89ff899aade844c3282c79f221f70154
Damián Chaban <mac@MacBookArch.localdomain>
2022-11-25 13:57:47 +0100

    25 Nov 22

--- Seminár/kopec a info o ňom a tak.py
+++ Seminár/kopec a info o ňom a tak.py
@@ -5,17 +5,17 @@ c=tk.Canvas(height=300,width=500)
 c.pack()
 
 v=n=d=x=0
-y=randrange(300)
+y=randrange(270)
 while True:
     if x<500:
         xx=x+randrange(100)
-        yy=randrange(300)
+        yy=randrange(270)
         c.create_line(x,y,xx,yy)
         if yy>y:
-            v+=yy-y
+            n+=yy-y
             d+=sqrt(((xx-x)**2)+((yy-y)**2))
         else:
-            n+=y-yy
+            v+=y-yy
             d+=sqrt(((xx-x)**2)+((y-yy)**2))
         x=xx
         y=yy
