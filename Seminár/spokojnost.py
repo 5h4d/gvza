443633d7d044ef8ccd762e859979ce00a073da51
Damián Chaban <mac@MacBookArch.localdomain>
2022-12-09 13:17:23 +0100

    9 Dec 22

--- Seminár/spokojnost.py
+++ Seminár/spokojnost.py
@@ -1,18 +1,18 @@
-from tkinter import Canvas
-c=Canvas(width=500, height=300, bg='grey')
+from tkinter import Canvas, Button
+c=Canvas(width=300, height=150, bg='grey')
 c.pack()
-c.create_text(250, 50, text='Boli ste spokojní s týmto tu?', font='comicsans 20')
-c.create_rectangle(20,160,120,230,fill='blue')
-c.create_rectangle(380,160,480,230,fill='blue')
-c.create_text(70,195,text='yea', font='arial 30')
-c.create_text(430,195,text='ne',font='arial 30')
+c.create_text(150, 40, text='Boli ste spokojní s týmto tu?', font='comicsans 15')
 yee=open('spokojnost.txt','w')
 yee.close()
-def bruh(xy):
+def bruh():
     yee=open('spokojnost.txt','a')
-    if 20<=xy.x<=120 and 160<=xy.y<=230:
-        yee.write('spokojny typek \n')
-    elif 380<=xy.x<=480 and 160<=xy.y<=230:
-        yee.write('nespokojny typek \n')
+    yee.write('spokojny typek \n')
     yee.close()
-c.bind('<Button-1>', bruh)
+def briuh():
+    yee=open('spokojnost.txt','a')
+    yee.write('nespokojny typek \n')
+    yee.close()
+yae=Button(text='yea',command=bruh)
+yae.place(x=25,y=70)
+nae=Button(text='ne',command=briuh)
+nae.place(x=230,y=70)
