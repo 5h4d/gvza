bcf3fa220d7d616a2a57989bec44e3d9168d1cf7
Damián Chaban <mac@MacBookArch.localdomain>
2023-09-13 14:12:28 +0200

    13 Sep 23

---
+++ Seminár/opakovanie Oktáva začiatok II.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from random import choice as pick
+from random import randrange as rng
+from time import sleep
+c=tk.Canvas(width=500,height=500,bg='white')
+c.pack()
+
+
+farbi=['red','yellow','blue','magenta','green']
+def lodenky():
+    c.create_rectangle(0,250,500,500,fill='lightblue',outline='lightblue')
+    c.bind('<Button-1>',balonka)
+def balonka(xy):
+    x=xy.x
+    y=xy.y
+    if y>250:
+        c.create_rectangle(x-15,y,x+15,y+10,fill='brown',outline='brown')
+        c.create_rectangle(x-7,y,x-12,y-40,fill='brown',outline='brown')
+        c.create_polygon(x-7,y-40,x-7,y-20,x+20,y-30,fill=pick((farbi)),outline='black')
+    else:
+        c.create_oval(x-30,y-30,x+30,y+30,fill=pick((farbi)),outline='black')
+        c.create_polygon(x-13,y+38,x+13,y+38,x+7,y+53,x-7,y+53,fill='brown',outline='brown')
+        a=x-40
+        while a<x+30:
+            a+=10
+            c.create_line(a,y,x,y+38,fill='black',width=3)
+
+##########################################################################################
+#####################################  ŠETRIČ  ###########################################
+##########################################################################################
+
+def res(xy):
+    c.delete('setritka')
+    a=3
+    angle=0
+def setrofka():
+    global coje, angle, a
+    c.create_text(rng(500),rng(500),text=coje.get(),font='arial '+str(a),tags='setritka',angle=angle)
+    angle+=10
+    a+=2
+    print('a')
+    setrofka()
+def setric():
+    global coje, a, angle
+    coje=tk.Entry()
+    coje.pack()
+    c.pack()
+    c.bind('<Button-1>',res)
+    a=3
+    angle=0
+    setrofka()
+#NEJDE TO A JE MI TO JEDNO IDEM DOMOV
+
+#TÝM PÁDOM NEPÔJDE ANI TO TRETIE TAKŽE KAŠĽAŤ
