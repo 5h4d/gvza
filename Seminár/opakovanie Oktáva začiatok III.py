d7b03e5cf5eb3e582bf43a210f502f51b2040c1b
Damián Chaban <mac@MacBookArch.localdomain>
2023-09-20 13:56:30 +0200

    20 Sep 23

---
+++ Seminár/opakovanie Oktáva začiatok III.py
@@ -0,0 +1,33 @@
+###TROJUHOLNÍK###
+yea=input("trojuholník: ")
+for i in range(len(yea)):
+    print(yea[i]*(i+1))
+print()
+
+###PYRAMÍDA###
+yea=int(input("tak daj n: "))
+for i in range(yea):
+    print(' '*(yea-1-i)+'*'*(2*i+1))
+print()
+
+###CIFERNÝ SÚČET###
+csum=0
+yea=input("číslo na ciferné rozloženie a súčet pls: ")
+for i in range(len(yea)):
+    print('{}. cifra je: {}'.format(i+1,str(yea)[i]))
+    csum+=int(str(yea)[i])
+print('Ciferný súčet je: '+str(csum))
+print()
+
+###MOCNINY###
+yea=int(input("zadaj od: "))
+yae=int(input("zadaj do: "))
+n=len(str(yae**4))
+for i in range(yea,yae+1):
+    print('%-{}i%-{}i%-{}i%-{}i'.format(n,n,n,n) % (i,i**2,i**3,i**4))
+print()
+
+###NÁSOBILKA###
+yea=int(input("zadaj od: "))
+yae=int(input("zadaj do: "))
+print("toto nejde a neviem ako to spraviť, to je celkom smola, čo?")
