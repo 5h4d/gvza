f9a1c6db16f0b4b88664408f27c032c7088dd72d
Damián Chaban <dch0@duck.com>
2023-10-26 14:53:40 +0200

    26 Oct 23

--- Seminár/n-uholník s uhlopriečkami.py
+++ Seminár/n-uholník s uhlopriečkami.py
@@ -5,17 +5,8 @@ n=int(input("n: "))
 c=Canvas(height=500,width=500,bg='white')
 c.pack()
 
-ang=360/n
-curang=0
-bodi=[]
-for i in range(n+1):
-    bodi.append((250+200*cos(radians(curang)),250-200*sin(radians(curang))))
-    curang+=ang
+bodi=[(250+200*cos(radians(i*360/n)),250-200*sin(radians(i*360/n))) for i in range(n+1)]
 
 for i,j in bodi:
-    zvysk=[x for x in bodi] """táto časť je vpodstate nepotrebná, následujúci loop by mohol vpohode pracovať
-                               aj s celým bodi len by tam boli čiary ukazujúce samé na seba, čo sa deje aj
-                               teraz keď sa nad tým zamýšľam"""
-    zvysk.remove((i,j))
-    for x,y in zvysk:
+    for x,y in bodi:
         c.create_line(i,j,x,y)
