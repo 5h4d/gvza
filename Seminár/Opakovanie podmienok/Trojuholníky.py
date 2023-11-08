c2de8aa947f0675801bee2fba5be3ecaf741d5ad
Damián Chaban <dch0@duck.com>
2023-11-08 22:53:57 +0100

    8 Nov 23

---
+++ Seminár/Opakovanie podmienok/Trojuholníky.py
@@ -0,0 +1,36 @@
+from tkinter import Canvas
+from matplotlib import colors
+from random import choice
+from math import sqrt
+
+colors = list(colors.cnames.keys())
+
+
+def trojuholniky(a: tuple[int, int], b: tuple[int, int], c: tuple[int, int]):
+    """Rekurzívna funkcia vykresľujúca trojuholníky na základe súradníc najväčšieho"""
+    if (
+        min(
+            sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
+            for x, y in ((a, b), (b, c), (c, a))
+        )
+        >= 30
+    ):
+        can.create_polygon(a, b, c, fill=choice(colors))
+        Sbc = (b[0] + c[0]) // 2, (b[1] + c[1]) // 2
+        Sac = (a[0] + c[0]) // 2, (a[1] + c[1]) // 2
+        Sab = (a[0] + b[0]) // 2, (a[1] + b[1]) // 2
+        trojuholniky(Sbc, Sac, Sab)
+    else:
+        return None
+
+
+a = tuple(int(x) for x in input("a: ").split(","))
+b = tuple(int(x) for x in input("b: ").split(","))
+c = tuple(int(x) for x in input("c: ").split(","))
+
+can = Canvas(width=800, height=800)
+can.pack()
+
+trojuholniky(a, b, c)
+
+can.mainloop()
--- Seminár/Opakovanie podmienok/Česko.py
+++ Seminár/Opakovanie podmienok/Česko.py
@@ -1,16 +1,16 @@
 from tkinter import Canvas
 from random import randint as rng
 
-c=Canvas(width=360, height=260)
+c = Canvas(width=360, height=260)
 c.pack()
 
 for i in range(10000):
-    x=rng(10, 350)
-    y=rng(10, 250)
-    if y<130:
-        col='blue' if x<y else 'white'
+    x = rng(10, 350)
+    y = rng(10, 250)
+    if y < 130:
+        col = "blue" if x < y else "white"
     else:
-        col='blue' if 260-x>y else 'red'
-    c.create_oval(x-5, y-5, x+5, y+5, outline='', fill=col)
-    
+        col = "blue" if 260-x > y else "red"
+    c.create_oval(x-5, y-5, x+5, y+5, outline="", fill=col)
+
 c.mainloop()
