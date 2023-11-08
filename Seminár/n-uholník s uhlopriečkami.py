09c957ee5931f54f7da3722688202a6d94884399
Damián Chaban <dch0@duck.com>
2023-11-08 13:04:23 +0100

    zohľadnenie dlhého riadku

--- Seminár/n-uholník s uhlopriečkami.py
+++ Seminár/n-uholník s uhlopriečkami.py
@@ -5,7 +5,8 @@ n=int(input("n: "))
 c=Canvas(height=500,width=500,bg='white')
 c.pack()
 
-bodi=[(250+200*cos(radians(i*360/n)),250-200*sin(radians(i*360/n))) for i in range(n+1)]
+bodi=[(250+200*cos(radians(i*360/n)),
+       250-200*sin(radians(i*360/n))) for i in range(n+1)]
 
 for i,j in bodi:
     for x,y in bodi:
